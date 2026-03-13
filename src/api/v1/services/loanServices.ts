1a94a7687a33499912431bfaf9a9f65d4fb196fd
Ralph.Vitug <rralph@rrc.ca>
2026-03-13 14:06:18 -0500

    add: CRUD operation services

---
+++ src/api/v1/services/loanServices.ts
@@ -0,0 +1,22 @@
+
+
+
+export const getAllLoans = (): string[] => {
+    // Logic to process all items from the database
+    return ["Item 1", "Item 2"];
+};
+
+export const createLoan = (loan: string): string => {
+    // Logic to add a new item to the database
+    return "Item added";
+};
+
+export const updateLoan = (id: number, loan: string): string => {
+    // Logic to update an item in the database
+    return "Item updated";
+};
+
+export const deleteLoan = (id: number): string => {
+    // Logic to delete an item from the database
+    return "Item deleted";
+};
