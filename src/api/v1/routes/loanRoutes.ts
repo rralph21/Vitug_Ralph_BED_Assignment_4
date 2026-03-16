f9a5d58886aec3e38848fdff9a11aa7a1a94bb44
Ralph.Vitug <rralph@rrc.ca>
2026-03-15 19:38:39 -0500

    add: validation into routes

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,14 +1,20 @@
 import { Router } from 'express';
 import { createLoan, deleteLoan, getAllLoans, updateLoan } from "../controllers/loanControllers";
+import { validateRequest } from "../middleware/validate";
+import {
+    createLoanSchema,
+    updateLoanSchema,
+    loanIdSchema,
+} from "../ulits/loanValidation";
 
 const router: Router = Router();
 
 router.get("/loans", getAllLoans)
 
-router.post("/loans", createLoan);
+router.post("/loans", validateRequest(createLoanSchema), createLoan);
 
-router.put("/loan/:id", updateLoan);
+router.put("/loan/:id", validateRequest(updateLoanSchema), updateLoan);
 
-router.delete("/loan/:id", deleteLoan);
+router.delete("/loan/:id", validateRequest(loanIdSchema), deleteLoan);
 
 export default router;
